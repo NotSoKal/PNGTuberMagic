aec0acbc9534868aae5427a72b6af97009a6bd8d
NotSoKal <notsokaltv@gmail.com>
2022-01-09 13:14:22 +0200

    Add bounce animation and clean UI

--- src/script.js
+++ src/script.js
@@ -1,119 +1,123 @@
 let settings = {
-    characterMouthClosed: '',
-    characterMouthOpen: '',
-    backgroundColor: '#00FF00',
-    voiceThreshold: 50,
-    volumeInterval: 200,
-    darkMode: true,
+  characterMouthClosed: '',
+  characterMouthOpen: '',
+  backgroundColor: '#00FF00',
+  voiceThreshold: 50,
+  volumeInterval: 200,
+  darkMode: true,
+  bounceOnSpeech: false,
 };
 
-let settingMouthClosed = document.querySelector('#setting-char-mouth-closed');
-let settingMouthOpen = document.querySelector('#setting-char-mouth-open');
-let settingBackgroundColor = document.querySelector('#setting-background-color');
-let settingVoiceThreshold = document.querySelector('#setting-voice-threshold');
-let settingVolumeInterval = document.querySelector('#setting-volume-interval');
-let settingDarkMode = document.querySelector('#setting-dark-mode');
+const settingMouthClosed = document.querySelector('#setting-char-mouth-closed');
+const settingMouthOpen = document.querySelector('#setting-char-mouth-open');
+const settingBackgroundColor = document.querySelector('#setting-background-color');
+const settingVoiceThreshold = document.querySelector('#setting-voice-threshold');
+const settingVolumeInterval = document.querySelector('#setting-volume-interval');
+const settingDarkMode = document.querySelector('#setting-dark-mode');
+const settingBounceOnSpeech = document.querySelector('#setting-bounce-on-speech');
 
-let thresholdValue = document.querySelector('#threshold-value');
-let intervalValue = document.querySelector('#interval-value');
+const thresholdValue = document.querySelector('#threshold-value');
+const intervalValue = document.querySelector('#interval-value');
 
-let characterArea = document.querySelector('#character-area');
-let character = document.querySelector('#character');
-let characterOpen = document.querySelector('#character-open');
-let saveBtn = document.querySelector('#save-settings');
+const characterArea = document.querySelector('#character-area');
+const characterContainer = document.querySelector('#character-container');
+const character = document.querySelector('#character');
+const characterOpen = document.querySelector('#character-open');
+const saveBtn = document.querySelector('#save-settings');
 
-let start = document.querySelector('#start-movement');
-let stop = document.querySelector('#stop-movement');
-let appStatus = document.querySelector('#app-status');
+const start = document.querySelector('#start-movement');
+const stop = document.querySelector('#stop-movement');
+const appStatus = document.querySelector('#app-status');
 
-let micStatusText = document.querySelector('#mic-status');
+const micStatusText = document.querySelector('#mic-status');
 let intervalID = null;
 let intervalFunc = null;
+let speechActive = false;
 
 let charMouthClosed;
 let charMouthOpen;
 
 const readSettings = () => {
-    const savedSettings = localStorage.getItem('png-tuber-settings');
+  const savedSettings = localStorage.getItem('png-tuber-settings');
 
-    if (savedSettings) {
-        settings = JSON.parse(localStorage.getItem('png-tuber-settings'));
-    }
+  if (savedSettings) {
+    settings = JSON.parse(localStorage.getItem('png-tuber-settings'));
+  }
 
-    settingMouthClosed.value = settings.characterMouthClosed;
-    settingMouthOpen.value = settings.characterMouthOpen;
-    settingBackgroundColor.value = settings.backgroundColor;
-    settingVoiceThreshold.value = settings.voiceThreshold;
-    settingVolumeInterval.value = settings.volumeInterval;
-    settingDarkMode.checked = settings.darkMode;
+  settingMouthClosed.value = settings.characterMouthClosed;
+  settingMouthOpen.value = settings.characterMouthOpen;
+  settingBackgroundColor.value = settings.backgroundColor;
+  settingVoiceThreshold.value = settings.voiceThreshold;
+  settingVolumeInterval.value = settings.volumeInterval;
+  settingDarkMode.checked = settings.darkMode;
+  settingBounceOnSpeech.checked = settings.bounceOnSpeech;
 
-    if (settings.darkMode) {
-        document.body.classList.add('dark');
-    } else {
-        document.body.classList.remove('dark');
-    }
+  if (settings.darkMode) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
 
-    thresholdValue.textContent = settingVoiceThreshold.value;
-    intervalValue.textContent = settingVolumeInterval.value;
+  thresholdValue.textContent = settingVoiceThreshold.value;
+  intervalValue.textContent = settingVolumeInterval.value;
 
-    charMouthClosed = settings.characterMouthClosed;
-    charMouthOpen = settings.characterMouthOpen;
+  charMouthClosed = settings.characterMouthClosed;
+  charMouthOpen = settings.characterMouthOpen;
 
-    character.setAttribute('src', charMouthClosed);
-    characterOpen.setAttribute('src', charMouthOpen);
-    characterArea.setAttribute('style', `background: ${settings.backgroundColor}`);
+  character.setAttribute('src', charMouthClosed);
+  characterOpen.setAttribute('src', charMouthOpen);
+  characterArea.setAttribute('style', `background: ${settings.backgroundColor}`);
 };
 
-settingVoiceThreshold.addEventListener('change', e => {
-    thresholdValue.textContent = e.target.value;
+settingVoiceThreshold.addEventListener('change', (e) => {
+  thresholdValue.textContent = e.target.value;
 });
 
-settingVolumeInterval.addEventListener('change', e => {
-    intervalValue.textContent = e.target.value;
+settingVolumeInterval.addEventListener('change', (e) => {
+  intervalValue.textContent = e.target.value;
 });
 
-saveBtn.addEventListener('click', e => {
-    let newSettings = {
-        characterMouthClosed: settingMouthClosed.value,
-        characterMouthOpen: settingMouthOpen.value,
-        backgroundColor: settingBackgroundColor.value,
-        voiceThreshold: settingVoiceThreshold.value,
-        volumeInterval: settingVolumeInterval.value,
-        darkMode: settingDarkMode.checked,
-    };
+saveBtn.addEventListener('click', () => {
+  const newSettings = {
+    characterMouthClosed: settingMouthClosed.value,
+    characterMouthOpen: settingMouthOpen.value,
+    backgroundColor: settingBackgroundColor.value,
+    voiceThreshold: settingVoiceThreshold.value,
+    volumeInterval: settingVolumeInterval.value,
+    darkMode: settingDarkMode.checked,
+    bounceOnSpeech: settingBounceOnSpeech.checked,
+  };
 
-    localStorage.setItem('png-tuber-settings', JSON.stringify(newSettings));
-    readSettings();
+  localStorage.setItem('png-tuber-settings', JSON.stringify(newSettings));
+  readSettings();
 });
 
 readSettings();
 
-const getAverageVolume = bytes => {
-    let values = 0;
-    let average;
+const getAverageVolume = (bytes) => {
+  let values = 0;
 
-    let length = bytes.length;
+  const { length } = bytes;
 
-    for (let i = 0; i < length; i++) {
-        values += bytes[i];
-    }
+  for (let i = 0; i < length; i++) {
+    values += bytes[i];
+  }
 
-    average = values / length;
-    return average;
-}
+  return values / length;
+};
 
 navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-.then(stream => {
-    micStatusText.textContent = "";
+  .then((stream) => {
+    micStatusText.textContent = '';
 
     const audioContext = new AudioContext();
     const microphone = audioContext.createMediaStreamSource(stream);
-    
+
     const analyserNode = new AnalyserNode(audioContext, {
-        fftSize: 2048,
-        minDecibels: -127,
-        maxDecibels: 0,
-        smoothingTimeConstant: 0.5,
+      fftSize: 2048,
+      minDecibels: -127,
+      maxDecibels: 0,
+      smoothingTimeConstant: 0.5,
     });
 
     microphone.connect(analyserNode);
@@ -121,30 +125,48 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
     const volumeData = new Uint8Array(analyserNode.frequencyBinCount);
 
     intervalFunc = () => {
-        analyserNode.getByteFrequencyData(volumeData);
-        let volume = getAverageVolume(volumeData);
-        
-        if (volume >= settings.voiceThreshold) {
-            character.style.display = 'none';
-            characterOpen.style.display = 'block';
-        } else {
-            character.style.display = 'block';
-            characterOpen.style.display = 'none';
+      analyserNode.getByteFrequencyData(volumeData);
+      const volume = getAverageVolume(volumeData);
+
+      if (volume >= settings.voiceThreshold) {
+        if (speechActive) {
+          return;
         }
+
+        if (settings.bounceOnSpeech) {
+          characterContainer.classList.add('bounce');
+        }
+
+        character.classList.add('hidden');
+        characterOpen.classList.remove('hidden');
+        speechActive = true;
+      } else {
+        if (!speechActive) {
+          return;
+        }
+
+        character.classList.remove('hidden');
+        characterOpen.classList.add('hidden');
+        speechActive = false;
+      }
     };
-})
-.catch(err => {
-    micStatusText.textContent = "Failed to capture microphone audio :/ Make sure to give permission for this site to use the microphone!";
+  })
+  .catch((err) => {
+    micStatusText.textContent = 'Failed to capture microphone audio :/ Make sure to give permission for this site to use the microphone!';
     micStatusText.style.color = 'red';
     console.error(err);
+  });
+
+characterContainer.addEventListener('animationend', (e) => {
+  e.target.classList.remove('bounce');
 });
 
-start.addEventListener("click", () => {
-    intervalID = setInterval(intervalFunc, 100);
-    appStatus.textContent = "Running";
+start.addEventListener('click', () => {
+  intervalID = setInterval(intervalFunc, 100);
+  appStatus.textContent = 'Running';
 });
 
-stop.addEventListener("click", () => {
-    clearInterval(intervalID);
-    appStatus.textContent = "Stopped";
+stop.addEventListener('click', () => {
+  clearInterval(intervalID);
+  appStatus.textContent = 'Stopped';
 });
